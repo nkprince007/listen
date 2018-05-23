a484c821604abc66090d9e284a83bf4537d7f511
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-05-23 21:30:12 +0530

    Use fixed port from environment variable

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"os"
+
 	"github.com/micro/go-log"
 	"github.com/micro/go-web"
 	"gitlab.com/nkprince007/listen/handler"
 )
 
+func getEnv(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func main() {
+	// grab port from environment
+	port := getEnv("PORT", "8000")
+
 	// create new web service
 	service := web.NewService(
 		web.Name("go.micro.web.listen"),
 		web.Version("latest"),
+		web.Address(":"+port),
 	)
 
 	// register call handler
