9ef83b1ba0fd0f213f8f5f657f44b141fe6f5713
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-05-25 21:48:49 +0530

    helpers: Add request logger

--- helpers.go
+++ helpers.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 // RejectOtherMethods accepts only the chosen HTTP method
 func RejectOtherMethods(method string, h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == method {
+			// log the requests handled
+			defer func(begin time.Time) {
+				log.Printf("HTTP path=%s method=%s took=%s",
+					r.URL.String(), r.Method, time.Since(begin))
+			}(time.Now())
+
 			h.ServeHTTP(w, r)
 			return
 		}
